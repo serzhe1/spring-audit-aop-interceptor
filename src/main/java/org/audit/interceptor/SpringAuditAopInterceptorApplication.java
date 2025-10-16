599fbd7ab4e38b48555b4dcaed97a4fc04af813b
s.ladygin <s.a.ladygin@yandex.ru>
2025-10-16 21:16:13 +0300

    init project

---
+++ src/main/java/org/audit/interceptor/SpringAuditAopInterceptorApplication.java
@@ -0,0 +1,13 @@
+package org.audit.interceptor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAuditAopInterceptorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAuditAopInterceptorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/audit/interceptor/SpringAuditAopInterceptorApplicationTests.java
@@ -0,0 +1,13 @@
+package org.audit.interceptor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAuditAopInterceptorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
